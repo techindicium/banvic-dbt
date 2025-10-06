64e4c70f303ccbf7749284fdacda2c8be34d03c5
Rulyan Fernandes <89143208+ruly-f@users.noreply.github.com>
2025-10-06 23:37:38 +0200

    Update stg_erp__agencias.sql

--- models/staging/erp_banvic/stg_erp__agencias.sql
+++ models/staging/erp_banvic/stg_erp__agencias.sql
@@ -8,12 +8,13 @@ with
         select
             cast(cod_agencia as int) as pk_agencia
             , cast(cod_localidade as int) as fk_localidade
-            , cast(nome as varchar) as nome_agencia
-            , cast(endereco as varchar) as endereco_agencia
+            , cast(nome as string) as nome_agencia
+            , cast(endereco as string) as endereco_agencia
             , cast(data_abertura as date) as data_de_abertura
-            , cast(tipo_agencia as varchar) as tipo_agencia
+            , cast(tipo_agencia as string) as tipo_agencia
         from fonte_agencias
     )
 
 select *
 from renomeado
+
